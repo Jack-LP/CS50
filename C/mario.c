8388411b1bfd8498072649a489a3625157ee9c10
Jack Paget <jackpaget1@gmail.com>
2022-03-06 21:56:01 +0000

    added mario and cash

---
+++ C/mario.c
@@ -0,0 +1,25 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int height, row, col, space;
+   do{
+       height = get_int("Enter height: ");
+   }
+   while (height > 8 || height < 1);
+
+    for (row = 0; row < height; row++)
+    {
+        for (space = 0; space < height - row - 1; space++)
+        {
+            printf(" ");
+        }
+        for (col = 0; col <= row; col++)
+        {
+            printf("#");
+        }
+    printf("\n");
+    }
+
+}
